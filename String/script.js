b2433e8b5c27db702aa79a52f87c83769ec91cd4
Monu Singh <monusingh995606@gmail.com>
2026-01-13 23:38:24 +0530

    js concatnation

---
+++ String/script.js
@@ -0,0 +1,18 @@
+// *   Basic String
+// let fname = "Monu";
+// let mname = "Singh";
+// let lname = "Patel";
+
+//  let fullname = fname+" " + mname +" "+ lname ;
+// let fullname = `${fname} ${mname} ${lname}` ;
+
+// console.log(fullname)
+
+
+// console.log(`My full name is ${fullname}`)
+
+
+//?  Cerating string using string constructor  
+
+let str = new String("Hello world")
+console.log(str)
